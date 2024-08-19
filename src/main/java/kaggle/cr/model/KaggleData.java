76b6507b868dad4557c55f2571aed8dac655d2b5
bpmachs <bpmachs99@gmail.com>
2024-08-19 20:56:08 +0530

    "added lombok and generation strategy "

--- src/main/java/kaggle/cr/model/KaggleData.java
+++ src/main/java/kaggle/cr/model/KaggleData.java
@@ -3,15 +3,20 @@ package kaggle.cr.model;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "crime_data_1")
+@Data
 public class KaggleData {
 	
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long drNo;
 	
 	private LocalDateTime dateReported;
